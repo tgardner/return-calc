a0877c0507bde00db8f2257170ea1db26ade7d62
Trent Gardner <trent.gardner@gmail.com>
2019-01-27 13:17:07 +1030

    Added location list stub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LocationListComponent } from './location-list/location-list.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'locations', component: LocationListComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  {
+    path: '',
+    redirectTo: '/calculator',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,8 @@ import { TimePipe } from './time.pipe';
 import { CalculatorControlsComponent } from './calculator-controls/calculator-controls.component';
 import { DeployCalculatorComponent } from './deploy-calculator/deploy-calculator.component';
 import { RecycleCalculatorComponent } from './recycle-calculator/recycle-calculator.component';
+import { LocationListComponent } from './location-list/location-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,15 @@ import { RecycleCalculatorComponent } from './recycle-calculator/recycle-calcula
     TimePipe,
     CalculatorControlsComponent,
     DeployCalculatorComponent,
-    RecycleCalculatorComponent
+    RecycleCalculatorComponent,
+    LocationListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [TimePipe],
   bootstrap: [AppComponent]
