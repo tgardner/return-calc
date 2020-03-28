666c9b921e42ee9df75eaf95fad887bf64e42d3f
Trent Gardner <trent.gardner@gmail.com>
2020-03-29 01:13:56 +1030

    Aligned deployment calculator flights

--- src/app/deploy-calculator/deploy-calculator.component.ts
+++ src/app/deploy-calculator/deploy-calculator.component.ts
@@ -1,64 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Calculator } from '../calculator';
 import { TimePipe } from '../time.pipe';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Flight } from '../flight';
 
-const timeInterval = interval(50);
+const timeInterval = interval(100);
 
 @Component({
   selector: 'app-deploy-calculator',
   templateUrl: './deploy-calculator.component.html',
   styleUrls: ['./deploy-calculator.component.scss']
 })
-export class DeployCalculatorComponent implements OnInit {
-  private timer: any;
+export class DeployCalculatorComponent {
+  private timer: Subscription;
   public started: boolean = false;
   public recalled: boolean = false;
-  public arrivalTime: Date;
-  public remaining: number = 0;
+  public flight: Flight;
   public initial: string;
-  public flightTime: number;
 
   constructor(
     private timePipe: TimePipe,
     private router: Router,
     private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      if (params["arrivalTime"])
-        this.arrivalTime = new Date(params["arrivalTime"]);
-
-      this.started = params["started"] == "true";
-      this.recalled = params["recalled"] == "true";
-      if (this.timer) {
-        this.timer.unsubscribe();
-        this.timer = null;
-      }
-
-      if (this.started) {
-        this.timer = timeInterval.subscribe(() => {
-          this.tick();
-        });
-      }
-    });
-  }
-
   public calculate(calculator: Calculator): void {
-    this.flightTime = calculator.calculateFlightTime();
-    if (isNaN(this.flightTime) || this.flightTime === Infinity) {
+    var flightTime = calculator.calculateFlightTime();
+    if (isNaN(flightTime) || flightTime === Infinity) {
       this.initial = null;
       return;
     }
 
-    var result = this.timePipe.transform(this.flightTime);
-    this.initial = result;
+    this.flight = new Flight(flightTime);
+    this.initial = this.timePipe.transform(flightTime);
+    var params = this.route.snapshot.queryParams;
+    if (params["arrivalTime"])
+      this.flight.endTime = new Date(params["arrivalTime"]);
+
+    this.started = params["started"] == "true";
+    this.recalled = params["recalled"] == "true";
+
+    if (this.timer) {
+      this.timer.unsubscribe();
+      this.timer = null;
+    }
+
+    if (this.started) {
+      this.timer = timeInterval.subscribe(() => {
+        this.tick();
+      });
+    }
   }
 
   private navigate() {
     var data = {
-      arrivalTime: !this.arrivalTime ? "" : this.arrivalTime.toISOString(),
+      arrivalTime: this.flight?.endTime?.toISOString() || "",
       started: this.started,
       recalled: this.recalled
     };
@@ -78,40 +74,27 @@ export class DeployCalculatorComponent implements OnInit {
 
     // minutes are worth 60 seconds. Hours are worth 60 minutes.
     var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
-
-    var endTime = new Date();
-    endTime.setTime(endTime.getTime() + seconds * 1000);
-    this.arrivalTime = endTime;
+    this.flight.start(seconds);
     this.started = true;
     this.navigate();
   }
 
   public recall(): void {
     this.recalled = true;
-
-    var endTime = new Date();
-    endTime.setTime(endTime.getTime() + (this.flightTime - this.remaining) * 1000);
-    this.arrivalTime = endTime;
-
+    this.flight.recall();
     this.navigate();
   }
 
   public stop(): void {
     this.started = false;
     this.recalled = false;
-    this.remaining = 0;
-    this.arrivalTime = null;
+    this.flight.stop();
     this.navigate();
   }
 
   private tick(): void {
-    var now: Date = new Date();
-    if (this.arrivalTime < now) {
+    if (this.flight.remaining <= 0) {
       this.stop();
-      return;
     }
-
-    var remaining = (+this.arrivalTime - +now);
-    this.remaining = remaining / 1000;
   }
 }
--- src/app/flight.ts
+++ src/app/flight.ts
@@ -1,7 +1,10 @@
 export class Flight {
-    constructor(public flightTime: number) {
+    constructor();
+    constructor(flightTime: number)
+    constructor(flightTime?: number) {
+        this.flightTime = flightTime || 0;
     }
-
+    public flightTime: number;
     public endTime: Date;
 
     public get remaining(): number {
@@ -11,8 +14,19 @@ export class Flight {
         return Math.round((+this.endTime - +now) / 1000);
     }
 
-    public start(): void {
+    public start(remaining?: number): void {
+        remaining = remaining || this.flightTime;
         this.endTime = new Date();
-        this.endTime.setTime(this.endTime.getTime() + this.flightTime * 1000);
+        this.endTime.setTime(this.endTime.getTime() + remaining * 1000);
+    }
+
+    public recall(): void {
+        var endTime = new Date();
+        endTime.setTime(endTime.getTime() + (this.flightTime - this.remaining) * 1000);
+        this.endTime = endTime;
+    }
+
+    public stop(): void {
+        this.endTime = null;
     }
 }
