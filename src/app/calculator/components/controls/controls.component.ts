a826ca5c050352b9e0cc8b49f3c079455399d46e
Trent Gardner <trent.gardner@gmail.com>
2021-09-25 13:39:30 +0930

    Added 6x uni

--- src/app/calculator/components/controls/controls.component.ts
+++ src/app/calculator/components/controls/controls.component.ts
@@ -12,7 +12,7 @@ export class ControlsComponent {
 
   @Output() change: EventEmitter<Calculator> = new EventEmitter<Calculator>();
 
-  public readonly modifiers: number[] = [1, 2, 3, 4, 5];
+  public readonly modifiers: number[] = [1, 2, 3, 4, 5, 6];
   public readonly speeds: number[] = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1];
   public readonly admiralBonuses: number[] = [0, .1, .2];
 
